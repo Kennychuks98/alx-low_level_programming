3099e0cdf8f9ca934dca84f0feffd4a5c316326b
Kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-09 09:42:06 -0700

    A c program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - this function is the entry point of the program
+ * return: Always return (0) if the program runs successfully
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
