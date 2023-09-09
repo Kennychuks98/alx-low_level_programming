09f02ef954e6f6260aae33f463a76e4806e1958b
Kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-09 09:23:28 -0700

    A c program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-/* This program demonstrates how to print a message in C.
-It prints the message "Programming is like building a multilingual puzzle" */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
