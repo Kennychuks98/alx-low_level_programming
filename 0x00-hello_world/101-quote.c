1d612b16e905dd768e2be01ed7ce5a14e037fc85
Kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-09 14:16:35 -0700

    a c program that prints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 int main(void)
 {
 char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, quote, 59);  
-/* Write the text to standard error (file descriptor 2) */
+write(2, quote, 59); /* Write the text to standard error (file descriptor 2) */
 return (1);
 }
 
