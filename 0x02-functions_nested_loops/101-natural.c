589668bded4bf1e0148b40defa2a8558fa9e25e1
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-13 19:36:28 -0700

    natural numbers that are multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,24 @@
 /**
 *main - Entry point
 *
-*Return: Always 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
 int sum = 0;
-int i;
-for (i = 0; i < 1024; i++)
+int num;
+for (num = 0; num < 1024; num++)
 {
-if (i % 3 == 0 || i % 5 == 0)
+if (num % 3 == 0 || num % 5 == 0)
 {
-sum += i;
+sum += num;
 }
 }
-_putchar((sum / 1000) + '0'); /* Print thousands digit */
-_putchar(((sum / 100) % 10) + '0'); /* Print hundreds digit */
-_putchar(((sum / 10) % 10) + '0'); /* Print tens digit */
+_putchar((sum / 100000) + '0'); /* Print hundreds of thousands digit */
+_putchar((sum / 10000) % 10 + '0'); /* Print ten-thousands digit */
+_putchar((sum / 1000) % 10 + '0'); /* Print thousands digit */
+_putchar((sum / 100) % 10 + '0'); /* Print hundreds digit */
+_putchar((sum / 10) % 10 + '0'); /* Print tens digit */
 _putchar((sum % 10) + '0'); /* Print ones digit */
 _putchar('\n');
 return (0);
