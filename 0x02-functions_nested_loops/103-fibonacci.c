869817eb3a135d7d03b6d569d5f5a73fbe670657
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-13 19:46:29 -0700

    a function in which each two functions is generated by two new terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+long int a = 1, b = 2, next, sum = 0;
+while (a <= 4000000)
+{
+if (a % 2 == 0)
+sum += a;
+    next = a + b;
+a = b;
+b = next;
+}
+printf("%ld\n", sum);
+return (0);
+}
+
