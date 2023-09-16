3a36c7b5cc1fc95e25ced821cc1bfc7b3d5cf8ed
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-15 18:58:25 -0700

    100 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,18 @@
-#include "main.h"
-/**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+#include <stdio.h>
 int main(void)
 {
-int i;
-long int a = 1, b = 2, next;
-for (i = 0; i < 50; i++)
+long int first = 1, second = 2, next, i;
+printf("%ld, %ld, ", first, second);
+for (i = 3; i <= 50; ++i)
 {
-if (i == 49) /* Last number, no comma and space */
-printf("%ld\n", a);
-else
-printf("%ld, ", a);
-next = a + b;
-a = b;
-b = next;
+next = first + second;
+printf("%ld", next);
+if (i < 50)
+printf(", ");
+first = second;
+second = next;
 }
+printf("\n");
 return (0);
 }
 
