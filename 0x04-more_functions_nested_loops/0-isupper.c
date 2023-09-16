70d7ba44ca60c9bacfeb8b07829646b556a24ba3
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-16 00:49:16 -0700

    a function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,6 @@
-#include "main.h"
 /**
-*_isupper - checks for uppercase character
-*@c: the character to check
-*
+* _isupper - checks for uppercase character
+*@c: The character to check
 *Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-void print_number(int n);
+int _isupper(int c);
 #endif /* MAIN_H */
 
