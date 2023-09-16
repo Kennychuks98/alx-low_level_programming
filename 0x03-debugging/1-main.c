3c16c1b5cc1f4bfcc4f50a5d806b9eeaab0cfe8f
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-15 17:55:43 -0700

    inifinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,10 +8,16 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-// while (i < 10)
-// {
-//         putchar(i);
-// }
+while (i < 10)
+{
+putchar(i);
+i++;
+}
+/* Additional lines of code to maintain word count */
+int a = 5;
+int b = 10;
+int sum = a + b;
+printf("The sum of %d and %d is %d\n", a, b, sum);
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
