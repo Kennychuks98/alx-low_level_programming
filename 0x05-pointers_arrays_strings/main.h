4fc402272d80eeb08aa725461bc6dcfe17096230
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-18 18:47:03 -0700

    a function that prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,15 @@
 #define MAIN_H
 #include <unistd.h>
 int _putchar(char c);
+/* Task 0 */
 void reset_to_98(int *n);
+/* Task 1 */
 void swap_int(int *a, int *b);
+/* Task 2 */
 int _strlen(char *s);
+/* Task 3 */
+void _puts(char *str);
+/* Task 4 */
+void print_rev(char *s);
 #endif /* MAIN_H */
 
