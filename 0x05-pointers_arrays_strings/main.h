88905f1fb4c9a1b493891b4185ce3d2c8b2602ee
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-19 18:36:55 -0700

    a function that prints n elements of an array of integers, followed by a new line

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,48 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* _putchar.c */
 int _putchar(char c);
-void _puts(char *str);
+/* 0-isupper.c */
+int _isupper(int c);
+/* 1-isdigit.c */
+int _isdigit(int c);
+/* 2-mul.c */
+int mul(int a, int b);
+/* 3-print_numbers.c */
+void print_numbers(void);
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+/* 5-more_numbers.c */
+void more_numbers(void);
+/* 6-print_line.c */
+void print_line(int n);
+/* 7-print_diagonal.c */
+void print_diagonal(int n);
+/* 8-print_square.c */
+void print_square(int size);
+/* 9-fizz_buzz.c */
+void fizz_buzz(void);
+/* 10-print_triangle.c */
+void print_triangle(int size);
+/* 100-prime_factor.c */
+void print_factors(long n);
+/* 101-print_number.c */
+void print_number(int n);
+/* 101-keygen.c */
+char *generate_password(void);
+/* 4-rev_array.c */
+void reverse_array(int *a, int n);
+/* 4-puts.c */
+int _puts(char *str);
+/* 4-print_rev.c */
 void print_rev(char *s);
+/* 5-rev_string.c */
 void rev_string(char *s);
+/* 6-puts2.c */
 void puts2(char *str);
+/* 7-puts_half.c */
 void puts_half(char *str);
-int _atoi(char *s);
+/* 8-print_array.c */
+void print_array(int *a, int n);
 #endif /* MAIN_H */
 
