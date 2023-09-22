ef59079bdcb7b76a30626c6574b5b014fafc05c6
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-21 23:38:19 -0700

    a function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,14 @@
 #include <unistd.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *rot13(char *str);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+void leet(char *);
+void print_number(int n);
+char *rot13(char *s);
 #endif /* MAIN_H */
 
