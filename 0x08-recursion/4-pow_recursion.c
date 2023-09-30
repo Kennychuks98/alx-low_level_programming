1b33442306d0321760e742554f5e775b1f674d89
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-30 06:19:13 -0700

    a function that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 *@x: The base number.
 *@y: The exponent.
 *
-*Return: The result of x raised to the power of y,
+*Return: (The result of x raised to the power of y),
 * or -1 if y is negative.
 */
 int _pow_recursion(int x, int y)
@@ -11,7 +11,7 @@ int _pow_recursion(int x, int y)
 if (y < 0) /* If y is negative, return -1 to indicate an error */
 return -1;
 if (y == 0) /* Base case: Any number raised to the power of 0 is 1 */
-return 1;
-return x * _pow_recursion(x, y - 1); /* Recursive call to compute power */
+return (1);
+return (x * _pow_recursion(x, y - 1)); /* Recursive call to compute power */
 }
 
