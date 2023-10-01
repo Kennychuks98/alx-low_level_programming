949aa5cc06ccc9be03157daa5cc2630353e4e19d
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-30 18:08:08 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,29 @@
-int is_prime_number(int n) {
-int i;
-if (n < 2) {
-return (0); /* Not a prime number */
+int is_prime_helper(int n, int i);
+/**
+*is_prime_number - checks if a number is prime
+*@n: the number to check
+*
+*Return: 1 if prime, 0 otherwise
+*/
+int is_prime_number(int n)
+{
+if (n <= 1)
+return 0;
+return is_prime_helper(n, 2);
 }
-for (i = 2; i < n; i++) {
-if (n % i == 0) {
-return (0); /* Not a prime number */
-}
-}
-return (1); /* Prime number */
+/**
+*is_prime_helper - helper function for is_prime_number
+*@n: the number to check
+*@i: the divisor to test
+*
+*Return: 1 if prime, 0 otherwise
+*/
+int is_prime_helper(int n, int i)
+{
+if (i == n)
+return 1;
+if (n % i == 0)
+return 0;
+return is_prime_helper(n, i + 1);
 }
 
