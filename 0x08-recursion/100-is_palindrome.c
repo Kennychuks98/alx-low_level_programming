9ebdd332bd0bf78e6527f4ffa3ee80300657eba4
kennychuks98 <Kenechuksaginam@gmail.com>
2023-09-30 18:28:12 -0700

    a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int is_palindrome(char *s)
 int len = 0;
 while (s[len] != '\0')
 len++;
-return (is_palindrome_helper(s, 0, len - 1));
+return is_palindrome_helper(s, 0, len - 1);
 }
 /**
 *is_palindrome_helper - helper function for is_palindrome
@@ -23,9 +23,9 @@ return (is_palindrome_helper(s, 0, len - 1));
 int is_palindrome_helper(char *s, int start, int end)
 {
 if (start >= end)
-return (1);
+return 1;
 if (s[start] != s[end])
-return (0);
-return (is_palindrome_helper(s, start + 1, end - 1));
+return 0;
+return is_palindrome_helper(s, start + 1, end - 1);
 }
 
