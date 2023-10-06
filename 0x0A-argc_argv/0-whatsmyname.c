5d1ee6c20eabda16dd6abe0f3c4b27e84c2b714c
kennychuks98 <Kenechuksaginam@gmail.com>
2023-10-06 10:34:30 -0700

    a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+/*
+*File: 0-whatsmyname.c
+*Authored by: [Your Name]
+*Date: [Date]
+*
+*Description:
+*This program prints its own name (the name of the executable) to the standard output.
+*It uses the 'argv' array to access the program name, making it dynamic even if the
+*executable is renamed.
+*
+*Usage: ./program_name
+*/
 int main(int argc, char *argv[])
 {
 if (argc > 0)
