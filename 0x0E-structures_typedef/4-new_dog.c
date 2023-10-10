e59bc08bc5558997b2b1d50a1c45d13cc36cfcd3
kennychuks98 <Kenechuksaginam@gmail.com>
2023-10-10 05:39:44 -0700

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,45 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
-*new_dog - Creates a new dog.
+*_strdup - Returns a pointer to a newly allocated space in memory,
+*which contains a copy of the string given as a parameter.
+*@str: The string to copy.
+*
+*Return: If str == NULL, insufficient memory, or if memory allocation
+*fails, return NULL; otherwise, return a pointer to the duplicated string.
+*/
+char *_strdup(char *str)
+{
+char *dup;
+int len, i;
+if (str == NULL)
+return (NULL);
+len = 0;
+while (str[len])
+len++;
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+dup[i] = str[i];
+return (dup);
+}
+/**
+*new_dog - Creates a new dog structure.
 *@name: The name of the dog.
 *@age: The age of the dog.
 *@owner: The owner of the dog.
 *
-*Return: A pointer to the new dog, or NULL if it fails.
+*Return: If the function fails - NULL.
+*Otherwise - a pointer to the newly created dog_t struct.
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog_ptr;
-char *new_name, *new_owner;
-int name_len = 0, owner_len = 0;
-while (name[name_len])
-name_len++;
-while (owner[owner_len])
-owner_len++;
- new_name = malloc(name_len + 1);
-new_owner = malloc(owner_len + 1);
-if (new_name == NULL || new_owner == NULL)
+dog_t *new_dog;
+char *dup_name, *dup_owner;
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+dup_name = _strdup(name);
+if (dup_name == NULL)
 {
-free(new_name);
-free(new_owner);
+free(new_dog);
 return (NULL);
 }
-for (int i = 0; i <= name_len; i++)
-new_name[i] = name[i];
-for (int i = 0; i <= owner_len; i++)
-new_owner[i] = owner[i];
-new_dog_ptr = malloc(sizeof(dog_t));
-if (new_dog_ptr == NULL)
+dup_owner = _strdup(owner);
+if (dup_owner == NULL)
 {
-free(new_name);
-free(new_owner);
+free(dup_name);
+free(new_dog);
 return (NULL);
 }
-new_dog_ptr->name = new_name;
-new_dog_ptr->age = age;
-new_dog_ptr->owner = new_owner;
-return (new_dog_ptr);
+new_dog->name = dup_name;
+new_dog->age = age;
+new_dog->owner = dup_owner;
+return (new_dog);
 }
 
